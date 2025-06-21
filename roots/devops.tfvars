4ff91026c3e2a9e78e1e2bd1fb1e324102f11cbd
Elenka76 <elenkal767@gmail.com>
2025-06-21 15:20:35 -0500

    Created vpc infra for task 2

--- roots/devops.tfvars
+++ roots/devops.tfvars
@@ -1,2 +1,14 @@
+#task_1
 github_org  = "Elenka76"
 github_repo = "rsschool-devops-course-tasks"
+#VPC-infra ## task_2
+project_name          = "rolling-scopes-school"
+vpc_cidr              = "10.0.0.0/16"
+public_subnets        = ["10.0.1.0/24", "10.0.2.0/24"]
+availability_zones    = ["us-east-1a", "us-east-1b"]
+private_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]
+ami_bastion           = "ami-09e6f87a47903347c"
+bastion_instance_type = "t3.micro"
+key_name              = "rs-school-project"
+ami_privare_instance  = "ami-09e6f87a47903347c"
+private_instance_type = "t3.micro"
--- roots/variables.tf
+++ roots/variables.tf
@@ -1,7 +1,55 @@
+#task_1
 variable "github_org" {
   type = string
 }
 
 variable "github_repo" {
   type = string
+}
+
+#VPC-infra
+
+variable "project_name" {
+  description = "The name of a project"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "A list of CIDR blocks for the public subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "A list of availability zones in which to create subnets"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "A list of CIDR blocks for the private subnets"
+  type        = list(string)
+}
+
+variable "ami_bastion" {
+  description = "ami id for bastion_server"
+}
+
+variable "bastion_instance_type" {
+  description = "instance_type for bastion_server"
+}
+
+variable "key_name" {
+  description = "key_name to ssh into bastion"
+}
+
+variable "ami_privare_instance" {
+  description = "ami id for bastion_server"
+}
+
+variable "private_instance_type" {
+  description = "instance_type for bastion_server"
 }
